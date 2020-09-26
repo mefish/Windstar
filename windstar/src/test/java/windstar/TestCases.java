6e71eaf110f199f8722ea4b6321311fc2ae6481e
Matthew Evan Fisher <matt.e.fisher@gmail.com>
2020-09-26 09:20:26 -0700

    Initial project transfer, in process of configuring gradle

---
+++ windstar/src/test/java/windstar/TestCases.java
@@ -0,0 +1,25 @@
+package windstar;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class TestCases {
+
+	@Test
+	void testsWillRun() {
+		assertTrue(true);
+	}
+	
+	@Test
+	void willPrintWelcomeMessage() {
+		var game = new Game();
+		assertTrue(game.GetWelcomeMessage() == "Welcome!");
+	}
+	
+	@Test
+	void canViewARoomDescription() {
+		var game = new Game();
+		assertTrue(game.GetRoom() != "");
+	}
+
+}
